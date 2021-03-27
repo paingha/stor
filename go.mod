292b0fcb3f3d7a04c9f60887ce87dbadd63bc4b1
paingha <apaingha@gmail.com>
2021-03-27 12:40:21 -0500

    changes and implemented interface for different db types

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go v1.38.7
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
+	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067
 	github.com/lib/pq v1.10.0
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 )
--- stor.go
+++ stor.go
@@ -1,16 +1,21 @@
-package main
+package stor
 
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
-	//_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type DatabaseConnection interface {
+	connectDB() (*sql.DB, error)
+}
+
 // DatabaseParams - parameters needed to create a new database connection
-type DatabaseParams struct {
+type DatabasePostgresParams struct {
 	host     string
 	port     int
 	user     string
@@ -20,48 +25,74 @@ type DatabaseParams struct {
 	ssl      string
 }
 
-func createDbConnectionString(databaseInput DatabaseParams) map[string]string {
-	switch databaseInput.dbtype {
-	case "postgres":
-		return map[string]string{
-			"databaseType":     "postgres",
-			"connectionString": fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", databaseInput.host, databaseInput.port, databaseInput.user, databaseInput.password, databaseInput.dbname),
-		}
-	case "mysql":
-		return map[string]string{
-			"databaseType":     "mysql",
-			"connectionString": fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", databaseInput.user, databaseInput.password, databaseInput.host, databaseInput.port, databaseInput.dbname),
-		}
-	case "sqlite3":
-		return map[string]string{
-			"databaseType":     "sqlite3",
-			"connectionString": fmt.Sprintf("./%s?_auth&_auth_user=%s&_auth_pass=%s", databaseInput.host, databaseInput.user, databaseInput.password),
-		}
-	default:
-		return map[string]string{
-			"databaseType":     "postgres",
-			"connectionString": fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", databaseInput.host, databaseInput.port, databaseInput.user, databaseInput.password, databaseInput.dbname),
-		}
-	}
+// DatabaseParams - parameters needed to create a new database connection
+type DatabaseMySqlParams struct {
+	host     string
+	port     int
+	user     string
+	password string
+	dbname   string
+	dbtype   string
+	ssl      string
 }
 
-func connectToDatabase(databaseInput DatabaseParams) {
-	db, err := sql.Open(createDbConnectionString(databaseInput)["databaseType"], createDbConnectionString(databaseInput)["connectionString"])
+// DatabaseParams - parameters needed to create a new database connection
+type DatabaseSqlite3Params struct {
+	user     string
+	password string
+	dbname   string
+	dbtype   string
+}
+
+func (d DatabasePostgresParams) connectDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", d.host, d.port, d.user, d.password, d.dbname, d.ssl))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer db.Close()
-	//check if db connected successfully
-	if err := db.Ping(); err != nil {
-		panic(err)
-	}
-	rows, err := db.Query("")
+	return db, nil
+}
+
+func (d DatabaseMySqlParams) connectDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", d.user, d.password, d.host, d.port, d.dbname))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer rows.Close()
+	return db, nil
+}
+
+func (d DatabaseSqlite3Params) connectDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", fmt.Sprintf("file:%s?_auth&_auth_user=%s&_auth_pass=%s", d.dbname, d.user, d.password))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func connectToDatabase(d DatabaseConnection) (*sql.DB, error) {
+	return d.connectDB()
 }
 
 func main() {
 	fmt.Println("Stor: A golang package for exporting databases and uploading to AWS S3, FTP, Google Drive and saving locally")
+	c := &DatabaseSqlite3Params{
+		dbtype:   "sqlite3",
+		dbname:   "mydb.db",
+		user:     "",
+		password: "",
+	}
+	conn, err := connectToDatabase(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(conn)
+	defer conn.Close()
+	//check if db connected successfully
+	if err := conn.Ping(); err != nil {
+		panic(err)
+	}
+	rows, err := conn.Query("")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
 }
